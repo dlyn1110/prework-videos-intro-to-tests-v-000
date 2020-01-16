754564f39328e930c08a0abe8f1619825ff2b655
Dani Lyn <dlyn1110@gmail.com>
2020-01-16 18:34:35 +0000

    Automatically backed up by Learn

--- conversions.rb
+++ conversions.rb
@@ -6,5 +6,6 @@ end
 
 def meal_choice(veg, starch,protein="meat")
   puts "What a nutritious meal!"
+  puts "A plate of #{protein} with #{veg} and #{starch}."
   return "A plate of #{protein} with #{veg} and #{starch}."
 end
